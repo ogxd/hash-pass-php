46babcbfcacf485f80a9f6b27b7aa398768cc3a6
Olivier Giniaux <oginiaux@gmail.com>
2019-02-01 21:37:07 +0100

    Add display password as png image

---
+++ get_password_png.php
@@ -0,0 +1,6 @@
+<?php
+$_GET['key'] = 1234;
+$_GET['service'] = "facebook";
+?>
+
+<img src="text_to_image.php?text=<?php include 'get_password.php'; ?>" id='captchaimg'>
---
+++ text_to_image.php
@@ -0,0 +1,22 @@
+<?php
+
+$text = $_GET["text"];
+$height = 50;
+
+$font = 'hack.ttf';
+$fontSize = 0.50 * $height;
+$width = 0.43 * $height * strlen($text);
+
+$im = imagecreatetruecolor($width, $height);	
+$textColor = imagecolorallocate($im, 0, 0, 0);			
+
+$backgroundColor = imagecolorallocate($im, 255, 255, 200);
+
+imagefill($im, 0, 0, $backgroundColor);	
+imagettftext($im, $fontSize, 0, 0, 0.75 * $height, $textColor, $font, $text);	
+
+header('Content-Type: image/png'); // Defining the image type to be shown in browser window
+imagepng($im); // Showing image
+imagedestroy($im); // Destroying image instance
+
+?>
